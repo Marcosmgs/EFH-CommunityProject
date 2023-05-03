8dea163b6084c571a3891953ca162748e18c3db7
marcosmgs <marcosmgs2023@gmail.com>
2023-05-03 19:33:50 +0000

    Created admin comment model panel to modarate comments

--- recipes/admin.py
+++ recipes/admin.py
@@ -11,3 +11,15 @@ class RecipeAdmin(SummernoteModelAdmin):
     prepopulated_fields = {'slug': ('title',)}
     list_filter = ('status', 'created_on')
     summernote_fields = ('method', 'ingredients')
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+
+    list_display = ('name', 'body', 'recipe', 'created_on', 'approved')
+    list_filter = ('approved', 'created_on')
+    search_fields = ('name', 'email', 'address', 'body')
+    actions = ['approve_comments']
+
+    def approve_comments(self, request, queryset):
+        queryset.updated(approved=True)
