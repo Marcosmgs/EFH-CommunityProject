0b8d08d5cd8db4b8c348c5f2f0631219509a8627
marcosmgs <marcosmgs2023@gmail.com>
2023-05-03 19:05:32 +0000

    created recipe datamodel

--- recipes/models.py
+++ recipes/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+STATUS = ((0, "Draft"), (1, "Published"))
+
+
+class Recipe(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="community_recipe")
+    updated_on = models.DateTimeField(auto_now=True)
+    description = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    prep_time = models.IntegerField()
+    cooking_time = models.IntegerField()
+    cost = models.IntegerField()
+    ingredients = models.TextField()
+    method = models.TextField()
+    featured_image = CloudinaryField('image', default='placeholder')
+    status = models.IntegerField(choices=STATUS, default=0)
+    excerpt = models.TextField(blank=True)
+    likes = models.ManyToManyField(User, related_name='recipe_likes', blank=True)
+
+    class Meta:
+        ordering = ['-created_on']
+
+    def __str__(self):
+        return self.title
+
+    def number_of_likes(self):
+        return self.likes.count()
+
+
