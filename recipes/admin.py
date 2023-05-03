cfa4549c7ae9eefcf615a03fa2ba0006d2a9b965
marcosmgs <marcosmgs2023@gmail.com>
2023-05-03 19:33:03 +0000

    Created admin recipe model panel to manage content

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Recipe, Comment
+from django_summernote.admin import SummernoteModelAdmin
 
-# Register your models here.
+
+@admin.register(Recipe)
+class RecipeAdmin(SummernoteModelAdmin):
+
+    list_display = ('title', 'slug', 'status', 'created_on')
+    search_fields = ['title', 'description']
+    prepopulated_fields = {'slug': ('title',)}
+    list_filter = ('status', 'created_on')
+    summernote_fields = ('method', 'ingredients')
