28797b59800b458d4f9934a3b7d5369c58dd1919
marcosmgs <marcosmgs2023@gmail.com>
2023-05-05 14:45:50 +0000

    Created recipe details head & linked up urls

--- recipes/urls.py
+++ recipes/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 
 
 urlpatterns = [
-    path('', views.RecipeList.as_view(), name='home')
+    path('', views.RecipeList.as_view(), name='home'),
+    path('<slug:slug>/', views.RecipeDetails.as_view(), name='recipe_details'),
 ]
