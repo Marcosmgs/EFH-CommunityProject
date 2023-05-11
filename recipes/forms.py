b52aa6b94b77352d0305752aefaff6f46a8d74f9
marcosmgs <marcosmgs2023@gmail.com>
2023-05-11 19:28:49 +0000

    Create class AddRecipeForm and define fields form

--- recipes/forms.py
+++ recipes/forms.py
@@ -6,3 +6,13 @@ class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ('body',)
+
+
+class AddRecipeForm(forms.ModelForm):
+
+    class Meta:
+        """
+        Get recipe model, choose fields to display
+        """
+        model = Recipe
+        fields = ('title', 'prep_time', 'cooking_time', 'description', 'ingredients', 'method', 'featured_image',)
