2ba76025cfbfca6917ad07633ca01b75494adf85
marcosmgs <marcosmgs2023@gmail.com>
2023-05-11 19:07:19 +0000

    Add autoslug and changed models field & updated req.txt

--- recipes/models.py
+++ recipes/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django_extensions.db.fields import AutoSlugField
 from cloudinary.models import CloudinaryField
 
 STATUS = ((0, "Draft"), (1, "Published"))
@@ -7,14 +8,13 @@ STATUS = ((0, "Draft"), (1, "Published"))
 
 class Recipe(models.Model):
     title = models.CharField(max_length=200, unique=True)
-    slug = models.SlugField(max_length=200, unique=True)
+    slug = AutoSlugField(populate_from='title', unique=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="community_recipe")
     updated_on = models.DateTimeField(auto_now=True)
     description = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     prep_time = models.IntegerField()
     cooking_time = models.IntegerField()
-    cost = models.IntegerField()
     ingredients = models.TextField()
     method = models.TextField()
     featured_image = CloudinaryField('image', default='placeholder')
@@ -42,6 +42,6 @@ class Comment(models.Model):
 
     class Meta:
         ordering = ['created_on']
-    
+
     def __str__(self):
         return f"Comment {self.body} by {self.name}"
