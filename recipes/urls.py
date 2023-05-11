20e67f08173a083653ca1263f939f8acb0f6c953
marcosmgs <marcosmgs2023@gmail.com>
2023-05-11 19:17:48 +0000

    Add navbar link to user add recipe & create url

--- recipes/urls.py
+++ recipes/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.RecipeList.as_view(), name='home'),
     path('<slug:slug>/', views.RecipeDetails.as_view(), name='recipe_details'),
     path('like/<slug:slug>', views.RecipeLike.as_view(), name='recipe_like'),
+    path('addrecipe', views.AddRecipe.as_view(), name='add_recipe'),
 ]
