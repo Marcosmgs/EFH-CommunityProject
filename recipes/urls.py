26982f9479c5da3d789d2c52719892be801b6e9d
marcosmgs <marcosmgs2023@gmail.com>
2023-05-11 19:58:12 +0000

    Created url for single recipe view & link recipe title

--- recipes/urls.py
+++ recipes/urls.py
@@ -4,8 +4,9 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.RecipeList.as_view(), name='home'),
-    path('<slug:slug>/', views.RecipeDetails.as_view(), name='recipe_details'),
+    path('recipes/<slug:slug>/', views.RecipeDetails.as_view(), name='recipe_details'),
     path('like/<slug:slug>', views.RecipeLike.as_view(), name='recipe_like'),
     path('addrecipe', views.AddRecipe.as_view(), name='add_recipe'),
     path('my_book', views.MyRecipeBook.as_view(), name='my_book'),
+    path('single/<slug:slug>/', views.SingleRecipe.as_view(), name='single'),
 ]
