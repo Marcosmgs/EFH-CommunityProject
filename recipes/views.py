0ce9ecc233b0f3b574ac4826574679618721c528
marcosmgs <marcosmgs2023@gmail.com>
2023-05-13 11:46:30 +0000

    Added code comments to each view and method on views.py

--- recipes/views.py
+++ recipes/views.py
@@ -8,6 +8,9 @@ from .forms import CommentForm, AddRecipeForm
 
 
 class RecipeList(generic.ListView):
+    """
+    This view is used to display paginated by 6 all recipes in the index page
+    """
     model = Recipe
     queryset = Recipe.objects.filter(status=1).order_by('-created_on')
     template_name = 'index.html'
@@ -15,8 +18,14 @@ class RecipeList(generic.ListView):
 
 
 class RecipeDetails(View):
-
+    """
+    This view is used to display recipe details including comments filter likes.
+    It also includes the comment form and add to meal plan form
+    """
     def get(self, request, slug, *args, **kwargs):
+        """
+        Retrive recipe and related comments and likes from the database
+        """
         queryset = Recipe.objects.filter(status=1)
         recipe = get_object_or_404(queryset, slug=slug)
         comments = recipe.comments.filter(approved=True).order_by('created_on')
@@ -37,6 +46,10 @@ class RecipeDetails(View):
         )
 
     def post(self, request, slug, *args, **kwargs):
+        """
+        Method called when a POST request is made to the view
+        in the comment form.
+        """
         queryset = Recipe.objects.filter(status=1)
         recipe = get_object_or_404(queryset, slug=slug)
         comments = recipe.comments.filter(approved=True).order_by('created_on')
@@ -71,6 +84,10 @@ class RecipeDetails(View):
 class RecipeLike(View):
 
     def post(self, request, slug):
+        """
+        Method called when a POST request is made to the view
+        via the like option.
+        """
         recipe = get_object_or_404(Recipe, slug=slug)
 
         if recipe.likes.filter(id=request.user.id).exists():
@@ -82,6 +99,9 @@ class RecipeLike(View):
 
 
 class AddRecipe(generic.CreateView):
+    """
+    View used to allow logged in users to create recipes
+    """
     model = Recipe
     template_name = 'add_recipe.html'
     form_class = AddRecipeForm
@@ -91,6 +111,7 @@ class AddRecipe(generic.CreateView):
         """
         Method called when valid form data has been posted.
         Signed user set as the author of the recipe.
+        set success message given to display
         """
         form.instance.author = self.request.user
         messages.success(self.request, "Recipe Added successfully")
@@ -98,19 +119,31 @@ class AddRecipe(generic.CreateView):
 
 
 class MyRecipeBook(generic.ListView):
+    """
+    View used to allow logged in users view a list of own created recipes
+    """
     model = Recipe
     queryset = Recipe.objects.order_by('created_on')
     template_name = 'my_book.html'
     paginate_by = 6
 
     def get_queryset(self):
-        """Override get_queryset to filter by user"""
+        """
+        Override get_queryset to filter by user
+        """
         return Recipe.objects.filter(author=self.request.user)
 
 
 class SingleRecipe(View):
-
+    """
+    View used to allow logged in users to view recipes
+    details for each one created
+    """
     def get(self, request, slug, *args, **kwargs):
+        """
+        Override get_queryset to filter by status 0
+        Get object recipe by slug field
+        """
         queryset = Recipe.objects.filter(status=0)
         recipe = get_object_or_404(queryset, slug=slug)
 
@@ -124,6 +157,9 @@ class SingleRecipe(View):
 
 
 class UpdateRecipe(generic.UpdateView):
+    """
+    View used to allow logged in users update already created recipes
+    """
     model = Recipe
     template_name = 'edit_recipe.html'
     form_class = AddRecipeForm
@@ -140,6 +176,9 @@ class UpdateRecipe(generic.UpdateView):
 
 
 class DeleteRecipe(generic.DeleteView):
+    """
+    View used to allow logged in users delete created recipes
+    """
     model = Recipe
     template_name = 'delete_recipe.html'
     success_url = reverse_lazy("my_book")
