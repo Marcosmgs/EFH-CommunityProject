b8f1624ea03800d43fef5b984301f7b1712b094f
marcosmgs <marcosmgs2023@gmail.com>
2023-05-16 09:36:33 +0000

    Fixed code identation and spacing in urls.py file

--- recipes/urls.py
+++ recipes/urls.py
@@ -4,11 +4,14 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.RecipeList.as_view(), name='home'),
-    path('recipes/<slug:slug>/', views.RecipeDetails.as_view(), name='recipe_details'),
+    path('recipes/<slug:slug>/',
+         views.RecipeDetails.as_view(), name='recipe_details'),
     path('like/<slug:slug>', views.RecipeLike.as_view(), name='recipe_like'),
     path('addrecipe', views.AddRecipe.as_view(), name='add_recipe'),
     path('my_book', views.MyRecipeBook.as_view(), name='my_book'),
     path('single/<slug:slug>/', views.SingleRecipe.as_view(), name='single'),
-    path('update/<slug:slug>', views.UpdateRecipe.as_view(), name='edit_recipe'),
-    path('delete/<slug:slug>', views.DeleteRecipe.as_view(), name='delete_recipe'),       
-]
+    path('update/<slug:slug>',
+         views.UpdateRecipe.as_view(), name='edit_recipe'),
+    path('delete/<slug:slug>',
+         views.DeleteRecipe.as_view(), name='delete_recipe'),
+         ]
